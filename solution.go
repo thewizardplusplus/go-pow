a476d3fe6fd008f9154b4f6c2544e2455b4e5a95
thewizardplusplus <thewizardplusplus@yandex.ru>
2025-04-08 16:59:48 +0500

    feat: :sparkles: Add the `Solution.Verify()` method

--- solution.go
+++ solution.go
@@ -1,6 +1,10 @@
 package pow
 
 import (
+	"bytes"
+	"errors"
+	"fmt"
+
 	powValueTypes "github.com/thewizardplusplus/go-pow/value-types"
 )
 
@@ -20,4 +24,29 @@ func (entity Solution) Nonce() powValueTypes.Nonce {
 
 func (entity Solution) HashSum() powValueTypes.HashSum {
 	return entity.hashSum
+}
+
+func (entity Solution) Verify() error {
+	targetBitIndex := entity.challenge.TargetBitIndex()
+
+	hashData, err := entity.challenge.hashDataLayout.Execute(ChallengeHashData{
+		Challenge:      entity.challenge,
+		TargetBitIndex: targetBitIndex,
+		Nonce:          entity.nonce,
+	})
+	if err != nil {
+		return fmt.Errorf("unable to execute the hash data layout: %w", err)
+	}
+
+	hashSum := entity.challenge.hash.ApplyTo(hashData)
+	if !bytes.Equal(hashSum.ToBytes(), entity.hashSum.ToBytes()) {
+		return errors.New("hash sums don't match")
+	}
+
+	target := makeTarget(targetBitIndex)
+	if !isHashSumFitTarget(hashSum, target) {
+		return errors.New("hash sum doesn't fit the target")
+	}
+
+	return nil
 }
