6c3a086ea270e5a60914984487c66ea710bc4eb3
thewizardplusplus <thewizardplusplus@yandex.ru>
2025-04-08 16:30:13 +0500

    feat: :sparkles: Add the `Challenge.Solve()` method

--- challenge.go
+++ challenge.go
@@ -1,9 +1,17 @@
 package pow
 
 import (
+	"fmt"
+
 	"github.com/samber/mo"
 	powValueTypes "github.com/thewizardplusplus/go-pow/value-types"
 )
+
+type ChallengeHashData struct {
+	Challenge      Challenge
+	TargetBitIndex int
+	Nonce          powValueTypes.Nonce
+}
 
 type Challenge struct {
 	leadingZeroCount powValueTypes.LeadingZeroCount
@@ -16,6 +24,10 @@ type Challenge struct {
 
 func (entity Challenge) LeadingZeroCount() powValueTypes.LeadingZeroCount {
 	return entity.leadingZeroCount
+}
+
+func (entity Challenge) TargetBitIndex() int {
+	return entity.hash.SizeInBits() - entity.leadingZeroCount.ToInt()
 }
 
 func (entity Challenge) CreatedAt() mo.Option[powValueTypes.CreatedAt] {
@@ -36,4 +48,53 @@ func (entity Challenge) Hash() powValueTypes.Hash {
 
 func (entity Challenge) HashDataLayout() powValueTypes.HashDataLayout {
 	return entity.hashDataLayout
+}
+
+func (entity Challenge) Solve() (Solution, error) {
+	targetBitIndex := entity.TargetBitIndex()
+	target := makeTarget(targetBitIndex)
+
+	nonce, err := powValueTypes.NewZeroNonce()
+	if err != nil {
+		return Solution{}, fmt.Errorf(
+			"unable to construct the zero initial nonce: %w",
+			err,
+		)
+	}
+
+	var hashSum powValueTypes.HashSum
+	for {
+		hashData, err := entity.hashDataLayout.Execute(ChallengeHashData{
+			Challenge:      entity,
+			TargetBitIndex: targetBitIndex,
+			Nonce:          nonce,
+		})
+		if err != nil {
+			return Solution{}, fmt.Errorf(
+				"unable to execute the hash data layout: %w",
+				err,
+			)
+		}
+
+		hashSum = entity.hash.ApplyTo(hashData)
+		if isHashSumFitTarget(hashSum, target) {
+			break
+		}
+
+		nonce, err = nonce.Incremented()
+		if err != nil {
+			return Solution{}, fmt.Errorf("unable to increment the nonce: %w", err)
+		}
+	}
+
+	solution, err := NewSolutionBuilder().
+		SetChallenge(entity).
+		SetNonce(nonce).
+		SetHashSum(hashSum).
+		Build()
+	if err != nil {
+		return Solution{}, fmt.Errorf("unable to build the solution: %w", err)
+	}
+
+	return solution, nil
 }
